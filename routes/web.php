bee4cd8bf59a430854dcaf6635052b6cc1da5d0e
Eliezer-Moo <fuuzze@gmail.com>
2023-07-14 11:59:37 -0600

    api laravel

---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::post('api/registro','App\Http\Controllers\UserController@register');
+Route::post('api/acceso','App\Http\Controllers\UserController@login');
+
+Route::resource('/api/carros', 'App\Http\Controllers\CarController');
